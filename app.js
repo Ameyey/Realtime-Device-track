cb0fdfe9f333c971bbb3abb34c8eabddc7e16c16
ameybahugune <asd>
2024-07-05 18:00:43 +0530

    create file are using a css and js

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express=require('express')
-const app=express()
+const app=express();
+const http =require("http");
+const socketio =require("socket.io");
+const path =require("path");
+const server=http.createServer(app);
+
+const io=socketio(server);
+
+app.set("view engine","ejs");
+app.set(express.static(path.join(__dirname, "public")));
 
 app.get("/",function(req,res){
   res.send("hey");
 });
 
-app.listen(3000);
+server.listen(3000);
 
